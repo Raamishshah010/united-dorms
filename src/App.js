1b389f7116791b8183769bfedc7c1191bf87bee7
Raamishshah010 <raamishshah010@gmail.com>
2023-03-01 22:30:54 +0500

    BoilerPlate Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
 import './App.css';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Auth/Login/Login';
+
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route to="/" element={<Login/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
