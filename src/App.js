06667484263a70639bb028b707ffb4522d18e000
Raamishshah010 <raamishshah010@gmail.com>
2023-03-01 22:56:26 +0500

    Working on login Page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route to="/" element={<Login/>} />
+          <Route path="/" element={<Login/>} />
         </Routes>
       </Router>
     </div>
--- src/components/Auth/Login/Login.jsx
+++ src/components/Auth/Login/Login.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import './Login.css';
+
+import { RiLockFill, RiMailFill } from "react-icons/ri";
 
 const Login = () => {
     return (
-        <div>Login</div>
+        <>
+        
+        <div className="LoginNavbar">
+            <h5>United Dorms</h5>
+        </div>
+        
+
+        <div className="loginForm">
+            <h5>Sign in to continue</h5>
+
+            <div className="loginField">
+                <span><RiMailFill/></span>
+                <input type="email" placeholder="Email Address" />
+            </div>
+
+            <div className="loginField">
+                <span><RiLockFill/></span>
+                <input type="password" placeholder="Password" />
+            </div>
+
+
+        </div>
+        
+        </>
     )
 }
 
