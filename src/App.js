b8bf6d1536b1d9b8b805d82af248cfe4fde813b7
Raamishshah010 <raamishshah010@gmail.com>
2023-03-02 17:42:01 +0500

    Working on Home

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Auth/Login/Login';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Login/>} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/login" element={<Login/>} />
         </Routes>
       </Router>
     </div>
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import "./Home.css"
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <>
+
+            {/* NAVBAR */}
+            <Navbar expand="lg" className="navbar">
+                <Container>
+                    <Navbar.Brand><Link className='navLogo'>United Dorms</Link> </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto navLinks">
+                            <Nav.Link className="navLink active"><Link className="navText" to="/">Home</Link></Nav.Link>
+                            
+                            <Nav.Link className="navLink"><Link className="navText" to="/">Gallery</Link></Nav.Link>
+                            
+                            <Nav.Link className="navLink"><Link className="navText" to="/">Testimonials</Link></Nav.Link>
+                            <Nav.Link className="navLink"><Link className="navText" to="/">About</Link></Nav.Link>
+                            <Nav.Link className="navLink"><Link className="navText" to="/">Blogs</Link></Nav.Link>
+                            
+                            
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+
+            {/* HERO SECTION */}
+            <section className='hero'>
+            <div className="container">
+
+            <h5>About <span className="heroText">United</span> Dorms</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure tempore alias consectetur at, velit minima aspernatur, voluptas, dicta porro commodi error enim a quia molestias animi ipsa dolor vero aut?</p>
+
+            <div className="heroBtn">
+                <button className='heroButtonOne'>Book Now</button>
+                <button className='heroButtonTwo'>Book Now</button>
+            </div>
+            </div>
+            </section>
+        </>
+    )
+}
+
+export default Home
