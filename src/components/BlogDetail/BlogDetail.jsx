892e62d5fe653f86120e66c39d2ea0c1cc5f3ef2
Raamishshah010 <raamishshah010@gmail.com>
2023-03-03 14:30:32 +0500

    Working on Blog Detail Done

--- src/components/BlogDetail/BlogDetail.jsx
+++ src/components/BlogDetail/BlogDetail.jsx
@@ -3,6 +3,7 @@ import './BlogDetail.css';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
 import why4 from './assets/why4.jpg';
+import Footer from '../utils/Footer/Footer';
 
 const BlogDetail = () => {
     return (
@@ -30,7 +31,7 @@ const BlogDetail = () => {
 
 
             {/* BLOG DETAIL MAIN */}
-            <section className="blogDetailMain" style={{ position: 'relative',backgroundImage: `linear-gradient(to bottom, #ffffff84 , #ffffff41) ,url(${why4})`, height: "60vh", backgroundRepeat: "no-repeat", width: "100%", backgroundSize: 'cover', backgroundPosition: 'center'}}>
+            <section className="blogDetailMain" style={{ position: 'relative', backgroundImage: `linear-gradient(to bottom, #ffffff84 , #ffffff41) ,url(${why4})`, height: "60vh", backgroundRepeat: "no-repeat", width: "100%", backgroundSize: 'cover', backgroundPosition: 'center' }}>
                 <div className="container">
                     <div className="blogDetailMainTitle">
                         <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, obcaecati? Iusto, molestiae!</h1>
@@ -41,14 +42,34 @@ const BlogDetail = () => {
             {/* BLOG DETAIL */}
             <section className="blogDetail mt-5">
 
-            <div className="container">
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus laboriosam atque amet porro facere perferendis cupiditate obcaecati recusandae maiores architecto minima, quos aperiam, incidunt similique modi dolore quibusdam ullam itaque. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta atque voluptatum autem ut maiores. Officiis reiciendis quas accusamus similique aliquam, laborum voluptatum repudiandae illo fugiat aperiam unde labore nisi quasi!</p>
-            <br />
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus laboriosam atque amet porro facere perferendis cupiditate obcaecati recusandae maiores architecto minima, quos aperiam, incidunt similique modi dolore quibusdam ullam itaque.</p>
-            </div>
+                <div className="container">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus laboriosam atque amet porro facere perferendis cupiditate obcaecati recusandae maiores architecto minima, quos aperiam, incidunt similique modi dolore quibusdam ullam itaque. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta atque voluptatum autem ut maiores. Officiis reiciendis quas accusamus similique aliquam, laborum voluptatum repudiandae illo fugiat aperiam unde labore nisi quasi!</p>
+                    <br />
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus laboriosam atque amet porro facere perferendis cupiditate obcaecati recusandae maiores architecto minima, quos aperiam, incidunt similique modi dolore quibusdam ullam itaque.</p>
+                    <br />
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus laboriosam atque amet porro facere perferendis cupiditate obcaecati recusandae maiores architecto minima, quos aperiam, incidunt similique modi dolore quibusdam ullam itaque.</p>
 
-            
+                    <div className="blogSecondImage">
+                        <img src={why4} width="100%" height="400px" alt="blogSecondImage" />
+                    </div>
+                    <br />
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus laboriosam atque amet porro facere perferendis cupiditate obcaecati recusandae maiores architecto minima, quos aperiam, incidunt similique modi dolore quibusdam ullam itaque. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta atque voluptatum autem ut maiores. Officiis reiciendis quas accusamus similique aliquam, laborum voluptatum repudiandae illo fugiat aperiam unde labore nisi quasi!</p>
+                    <br />
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus laboriosam atque amet porro facere perferendis cupiditate obcaecati recusandae maiores architecto minima, quos aperiam, incidunt similique modi dolore quibusdam ullam itaque.</p>
+                    <br />
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus laboriosam atque amet porro facere perferendis cupiditate obcaecati recusandae maiores architecto minima, quos aperiam, incidunt similique modi dolore quibusdam ullam itaque.</p>
+
+                </div>
+
+
+
             </section>
+
+
+
+
+            {/* FOOTER */}
+            <Footer />
         </>
     )
 }
