9f4a933bb0f2122980e3e963fc915d78b8e3df2c
Raamishshah010 <raamishshah010@gmail.com>
2023-03-04 23:24:02 +0500

    add dorm ui done

--- src/components/Admin/AddDorm/AddDorm.jsx
+++ src/components/Admin/AddDorm/AddDorm.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React from 'react';
+import logout from './assets/logout.png';
+import './AddDorm.css'
+
+import { MdKeyboardBackspace } from "react-icons/md";
+import { Link } from 'react-router-dom';
+import Footer from '../../utils/Footer/Footer';
 
 const AddDorm = () => {
     return (
-        <div>AddDorm</div>
+        <>
+            <div className="LoginNavbar">
+                <div className="backBtn float-start" >
+                    <span ><Link className="backIcon" to="/admin/dashboard"><MdKeyboardBackspace /></Link></span>
+                </div>
+                <h5>United Dorms</h5>
+
+                <div className="logoutButton">
+                    <img src={logout} alt="" />
+                </div>
+            </div>
+
+            <div className="addBlogMain">
+                <div className="container">
+                    <h1>Add New Dorm</h1>
+                    <div className="inputs">
+
+                        <div className="input">
+                            <input type="text" id='dormId' placeholder="Enter Dorm ID" />
+                        </div>
+
+                        <div className="input">
+                            <textarea name="" id="" cols="30" rows="10" placeholder="Enter Dorm Detail"></textarea>
+                        </div>
+
+                        <div className="input">
+                            <input type="text" id='dormId' placeholder="Enter Rent Details $$$" />
+                        </div>
+                        
+                        <div className="input">
+                            <button className='addDorm'>Save</button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        {/* FOOTER */}
+        <Footer/>
+        </>
     )
 }
 
