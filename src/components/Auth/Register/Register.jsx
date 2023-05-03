93eefc579e557ae97e097cd6ac54c554af15552f
Raamishshah010 <raamishshah010@gmail.com>
2023-05-03 18:26:13 +0500

    Working on Revisions

--- src/components/Auth/Register/Register.jsx
+++ src/components/Auth/Register/Register.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Register.css';
 
-import { RiLockFill, RiMailFill } from "react-icons/ri";
+import { RiLockFill, RiMailFill, RiUser3Fill } from "react-icons/ri";
 import { Link } from 'react-router-dom';
 import Footer from '../../utils/Footer/Footer';
 
@@ -18,12 +18,12 @@ const Register = () => {
             <h5>Sign in to continue</h5>
 
             <div className="loginField">
-                <span><RiMailFill/></span>
+                <span><RiUser3Fill/></span>
                 <input type="text" placeholder="Choose a usename" />
             </div>
 
             <div className="loginField">
-                <span><RiMailFill/></span>
+                <span><RiUser3Fill/></span>
                 <input type="text" placeholder="Enter your full name" />
             </div>
 
@@ -38,13 +38,12 @@ const Register = () => {
             </div>
                 <small className='small-text'>Passwords must have at least 8 characters and contain at least two of the following: uppercase letters, lowercase letters, numbers, and symbols.</small>
 
-            <span className="forgotLink">Forgot Password?</span>
 
             <div className="loginBtn">
-                <Link to="/" className="signInBtn">Sign In</Link>
+                <Link to="/login" className="signInBtn">Sign In</Link>
             </div>
 
-            <h6>New To United Dorms? <Link className='link' to="/register">Sign Up</Link></h6>
+            <h6>Already Have an Account? <Link className='link' to="/login">Sign in</Link></h6>
 
         </div>
 
